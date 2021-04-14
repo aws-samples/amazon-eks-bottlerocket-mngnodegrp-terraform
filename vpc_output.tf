b8a2a696dfcb770f16942193db3cf79ccc5012b1
Rahman <imtranur@38f9d3d0d50a.ant.amazon.com>
2021-04-14 14:17:19 -0500

    Added logic to show subnets id's on screen

--- vpc_output.tf
+++ vpc_output.tf
@@ -1,12 +1,9 @@
 output "vpc_network_id" {
   value = aws_vpc.eks_network.id
 }
-output "private_subnet_ids" {
-  value = aws_subnet.private.*.id
+output "private_subnet_id" {
+  value          = [for az, subnet in aws_subnet.private: subnet.id]
 }
-
-output "public_subnet_ids" {
-  value = aws_subnet.public.*.id
+output "public_subnet_id" {
+  value          = [for az, subnet in aws_subnet.public: subnet.id]
 }
-
-
